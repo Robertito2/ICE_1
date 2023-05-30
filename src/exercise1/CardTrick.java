a62ee0a5c0bf70b1e0889d93cf9b5688db070e14
Robert Orazu <orazurobert@yahoo.com>
2023-05-30 14:26:14 +0100

    Second commit on new local branch 9:18

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -46,7 +46,7 @@ public class CardTrick {
      * @author Robert Orazu May 2023
      */
     private static void printInfo() {
-    
+        //  I'm done   
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
