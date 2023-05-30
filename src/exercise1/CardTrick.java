068396d9a0a135e5f777080fff673a52fa5f1d68
Robert Orazu <orazurobert@yahoo.com>
2023-05-30 14:18:05 +0100

    Second commit on new local branch 9:18

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -6,46 +6,51 @@ package exercise1;
  * To be used as starting code in Exercise
  *
  * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Robert Orazu May 30, 2023
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
     
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
+        Random random = new Random();
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            card.setValue(random.nextInt(14));
+            card.setSuit(Card.SUITS[random.nextInt(3)]);
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
 
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Insert a number 0 - 13");
+        int input = keyboard.nextInt();
+        for (int i = 0; i < 7; i++) {
+            if(hand[i].getValue() == input){
+                printInfo();
+                break;      
+            }
+        }
+        System.out.println("Program is ending..."); 
     }
 
     /**
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * @author Robert Orazu May 2023
      */
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Robert, but you can call me Tito, Bob or Emerie");
         System.out.println();
         
         System.out.println("My career ambitions:");
@@ -55,13 +60,11 @@ public class CardTrick {
 
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
-        System.out.println("-- Cooking");
+        System.out.println("-- Money");
         System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Playing soccer");
 
-        System.out.println();
-        
-    
+        System.out.println("I am from Nigeria");
     }
 
 }
